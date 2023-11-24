369f9380af3557265f35df1b1a429a3fe715fc9b
brometheuser <95117135+brometheuser@users.noreply.github.com>
2023-11-24 14:00:35 +0800

    2023-11-24 liwr executor测试提交

--- src/test/java/com/herman/mybatis/test/ExecutorTest.java
+++ src/test/java/com/herman/mybatis/test/ExecutorTest.java
@@ -1,9 +1,13 @@
 package com.herman.mybatis.test;
 
+import com.alibaba.fastjson.JSON;
+import com.herman.mybatis.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.io.IOException;
@@ -19,17 +23,29 @@ import java.io.Reader;
  * @Version: 1.0
  */
 
+@Slf4j
 public class ExecutorTest {
 
 
+//      try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/call_setters_on_nulls_again/mybatis-config.xml")) {
+//    sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+//  }
+
   @BeforeAll
   public void createSqlSession() throws IOException {
-    String configfile = "com/herman/mybatis/test/mybatis-configuration.xml";
-    InputStream is = Resources.getResourceAsStream("com/herman/mybatis/test/mybatis-config.xml");
+    String configfile = "mybatis-configuration.xml";
+    InputStream is = Resources.getResourceAsStream(configfile);
     SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
     SqlSessionFactory factory = builder.build(is);
     SqlSession sqlSession = factory.openSession(true);
-    sqlSession.getMapper()
+    UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+    UserEntity user = mapper.findById(1);
+    log.info("user{}", JSON.toJSONString(user));
+  }
+
+  @Test
+  public void test(){
+
 
   }
 
